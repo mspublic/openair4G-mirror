d35ab5836c85d410d8b15cc46214ec7086b53a2a
pburlot <pburlot@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2009-01-21 11:52:32 +0000

    separation de la file de message CMM/RRM a envoyer en 2 files distinctes ( file_send_cmm_msg, file_send_rrc_msg)

--- openair_lte/openair3/MESH/RRM/mr_attach.c
+++ openair_lte/openair3/MESH/RRM/mr_attach.c
@@ -12,9 +12,9 @@
 
    
 \par     Historique:
-            $Author$  $Date$  $Revision$
-            $Id$
-            $Log$
+        P.BURLOT 2009-01-20 
+            + separation de la file de message CMM/RRM a envoyer en 2 files 
+              distinctes ( file_send_cmm_msg, file_send_rrc_msg)
 
 *******************************************************************************
 */
@@ -47,7 +47,9 @@
 #include "mr_attach.h"
 
 //! Met un message dans la file des messages a envoyer
-#define PUT_MSG(s,m)  put_msg(  &(rrm->file_send_msg),s,m) 
+#define PUT_CMM_MSG(m)  put_msg(  &(rrm->file_send_cmm_msg),rrm->cmm.s,m ) 
+#define PUT_PUSU_MSG(m) put_msg(  &(rrm->file_send_cmm_msg),rrm->pusu.s,m) 
+#define PUT_RRC_MSG(m)  put_msg(  &(rrm->file_send_rrc_msg),rrm->rrc.s,m ) 
 
 /*!
 *******************************************************************************
@@ -69,9 +71,9 @@ void rrc_MR_attach_ind(
         add_item_transact( &(rrm->rrc.transaction), rrm->rrc.trans_cnt ,INT_RRC,RRM_SENSING_MEAS_REQ,0,NO_PARENT );
         pthread_mutex_unlock( &( rrm->rrc.exclu ) ) ;
 
-        PUT_MSG( rrm->rrc.s,msg_rrm_sensing_meas_req( inst, L2_id ,Sensing_meas_desc, rrm->rrc.trans_cnt));
+        PUT_RRC_MSG( msg_rrm_sensing_meas_req( inst, L2_id ,Sensing_meas_desc, rrm->rrc.trans_cnt));
         
-        PUT_MSG( rrm->cmm.s,msg_rrm_MR_attach_ind(inst,L2_id ));
+        PUT_CMM_MSG( msg_rrm_MR_attach_ind(inst,L2_id ));
     }
     else
         fprintf(stderr,"[RRM] RRC_MR_ATTACH_IND  is not allowed (Only CH):etat=%d\n",rrm->state);   
@@ -134,7 +136,7 @@ void rrc_cx_establish_ind(
     
     if ( rrm->state == CLUSTERHEAD )
     {
-        PUT_MSG( rrm->cmm.s,msg_rrm_attach_ind(inst,L2_id,L3_info_t,L3_info, 0 )) ;
+        PUT_CMM_MSG( msg_rrm_attach_ind(inst,L2_id,L3_info_t,L3_info, 0 )) ;
     }
     else if ( rrm->state == MESHROUTER )
     {
@@ -147,7 +149,7 @@ void rrc_cx_establish_ind(
         add_item_transact( &(rrm->cmm.transaction), rrm->cmm.trans_cnt,INT_CMM,RRCI_ATTACH_REQ,Trans_id,PARENT);
         pthread_mutex_unlock( &( rrm->cmm.exclu ) ) ;       
 
-        PUT_MSG( rrm->cmm.s,msg_rrci_attach_req(inst,L2_id,L3_info_t,L3_info, DTCH_B_id, DTCH_id ,rrm->cmm.trans_cnt)) ;
+        PUT_CMM_MSG( msg_rrci_attach_req(inst,L2_id,L3_info_t,L3_info, DTCH_B_id, DTCH_id ,rrm->cmm.trans_cnt)) ;
     }
     else
         fprintf(stderr,"[RRM] RRC_CX_ESTABLISH_IND (%d) is not allowed (Only CH):etat=%d\n",Trans_id,rrm->state);   
@@ -172,7 +174,7 @@ void rrc_phy_synch_to_CH_ind(
         /* Memorisation du L2_id du noeud ( c'est le niveau RRC qui a l'info ) */
         memcpy( &rrm->L2_id,  &L2_id, sizeof(L2_ID));
 
-        PUT_MSG(    rrm->rrc.s,
+        PUT_RRC_MSG( 
                         msg_rrci_init_mr_req( inst, 
                                 rrm->rrc.trans_cnt,
                                 &Lchan_desc[QOS_SRB0], 
@@ -229,7 +231,7 @@ void cmm_attach_cnf(
                 pTransactParent =get_item_transact(rrm->rrc.transaction,parent_id ) ;
                 if ( pTransactParent != NULL )
                 {
-                    PUT_MSG( rrm->rrc.s, msg_rrci_cx_establish_resp(inst,pTransactParent->id,L2_id,L3_info,L3_info_t ));
+                    PUT_RRC_MSG( msg_rrci_cx_establish_resp(inst,pTransactParent->id,L2_id,L3_info,L3_info_t ));
                     del_item_transact( &(rrm->rrc.transaction),pTransactParent->id ) ;
                 }
                 else // la transaction parent est inconnue, on ne fait rien
@@ -268,7 +270,7 @@ void rrc_sensing_meas_ind(
             
         pthread_mutex_unlock( &( rrm->rrc.exclu )  ) ;
         // send the response to rrc 
-        PUT_MSG( rrm->rrc.s, msg_rrm_sensing_meas_resp(inst,Trans_id) );
+        PUT_RRC_MSG( msg_rrm_sensing_meas_resp(inst,Trans_id) );
 
         for (  i = 0 ; i< NB_meas ; i++)
         {
@@ -277,7 +279,7 @@ void rrc_sensing_meas_ind(
             add_item_transact( &(rrm->pusu.transaction), rrm->pusu.trans_cnt,INT_PUSU,RRM_SENSING_INFO_IND,0,NO_PARENT);
             pthread_mutex_unlock( &( rrm->pusu.exclu ) ) ;  
                 
-            PUT_MSG( rrm->pusu.s, msg_rrm_sensing_info_ind(inst, L2_id,Sensing_meas[i].L2_id, Sensing_meas[i].Rssi, rrm->pusu.trans_cnt ) ) ;
+            PUT_PUSU_MSG( msg_rrm_sensing_info_ind(inst, L2_id,Sensing_meas[i].L2_id, Sensing_meas[i].Rssi, rrm->pusu.trans_cnt ) ) ;
         }
 
     }
