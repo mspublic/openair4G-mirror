7e5481af30558aad8d8dc2ac4f619faba98dc8ee
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2009-11-06 12:23:08 +0000

    fixed some bugs in lte_sync_time

--- openair_lte/openair1/PHY/LTE_ESTIMATION/lte_sync_time.c
+++ openair_lte/openair1/PHY/LTE_ESTIMATION/lte_sync_time.c
@@ -10,13 +10,13 @@
 #include "PHY/extern.h"
 
 int* sync_corr = NULL;
-short sync0F_tmp[256*2] __attribute__((aligned(16)));
-short sync1F_tmp[256*2] __attribute__((aligned(16)));
-short sync2F_tmp[256*2] __attribute__((aligned(16)));
+short syncF_tmp[2048*2] __attribute__((aligned(16)));
+//short sync1F_tmp[256*2] __attribute__((aligned(16)));
+//short sync2F_tmp[256*2] __attribute__((aligned(16)));
 
 int lte_sync_time_init(LTE_DL_FRAME_PARMS *frame_parms, LTE_UE_COMMON *common_vars ) {
 
-  int i;
+  int i,k;
   //unsigned short ds = frame_parms->log2_symbol_size - 7;
   
   sync_corr = (int *)malloc16(LTE_NUMBER_OF_SUBFRAMES_PER_FRAME*sizeof(int)*frame_parms->samples_per_tti);
@@ -68,14 +68,10 @@ int lte_sync_time_init(LTE_DL_FRAME_PARMS *frame_parms, LTE_UE_COMMON *common_va
   }
 
   // generate oversampled sync_time sequences
-  int k=frame_parms->ofdm_symbol_size-36;
+  k=frame_parms->ofdm_symbol_size-36;
   for (i=0; i<73; i++) {
-    sync0F_tmp[2*k] = primary_synch0[2*i]>>2;  //we need to shift input to avoid overflow in fft
-    sync0F_tmp[2*k+1] = primary_synch0[2*i+1]>>2;
-    sync1F_tmp[2*k] = primary_synch1[2*i]>>2;  //we need to shift input to avoid overflow in fft
-    sync1F_tmp[2*k+1] = primary_synch1[2*i+1]>>2;
-    sync2F_tmp[2*k] = primary_synch2[2*i]>>2;  //we need to shift input to avoid overflow in fft
-    sync2F_tmp[2*k+1] = primary_synch2[2*i+1]>>2;
+    syncF_tmp[2*k] = primary_synch0[2*i]>>2;  //we need to shift input to avoid overflow in fft
+    syncF_tmp[2*k+1] = primary_synch0[2*i+1]>>2;
     k++;
     if (k >= frame_parms->ofdm_symbol_size) {
       k++;  // skip DC carrier
@@ -83,7 +79,7 @@ int lte_sync_time_init(LTE_DL_FRAME_PARMS *frame_parms, LTE_UE_COMMON *common_va
     }
   }
 
-  fft((short*)sync0F_tmp,          /// complex input
+  fft((short*)syncF_tmp,          /// complex input
       (short*)primary_synch0_time,          /// complex output
       frame_parms->twiddle_ifft,    /// complex twiddle factors
       frame_parms->rev,             /// bit reversed permutation vector
@@ -91,14 +87,37 @@ int lte_sync_time_init(LTE_DL_FRAME_PARMS *frame_parms, LTE_UE_COMMON *common_va
       frame_parms->log2_symbol_size/2,
       0);                            /// 0 - input is in complex Q1.15 format, 1 - input is in complex redundant Q1.15 format)
     
-  fft((short*)sync1F_tmp,          /// complex input
+  k=frame_parms->ofdm_symbol_size-36;
+  for (i=0; i<73; i++) {
+    syncF_tmp[2*k] = primary_synch1[2*i]>>2;  //we need to shift input to avoid overflow in fft
+    syncF_tmp[2*k+1] = primary_synch1[2*i+1]>>2;
+    k++;
+    if (k >= frame_parms->ofdm_symbol_size) {
+      k++;  // skip DC carrier
+      k-=frame_parms->ofdm_symbol_size;
+    }
+  }
+
+  fft((short*)syncF_tmp,          /// complex input
       (short*)primary_synch1_time,          /// complex output
       frame_parms->twiddle_ifft,    /// complex twiddle factors
       frame_parms->rev,             /// bit reversed permutation vector
       frame_parms->log2_symbol_size,/// log2(FFT_SIZE)
       frame_parms->log2_symbol_size/2,
       0) ;                           /// 0 - input is in complex Q1.15 format, 1 - input is in complex redundant Q1.15 format)
-  fft((short*)sync2F_tmp,          /// complex input
+
+  k=frame_parms->ofdm_symbol_size-36;
+  for (i=0; i<73; i++) {
+    syncF_tmp[2*k] = primary_synch2[2*i]>>2;  //we need to shift input to avoid overflow in fft
+    syncF_tmp[2*k+1] = primary_synch2[2*i+1]>>2;
+    k++;
+    if (k >= frame_parms->ofdm_symbol_size) {
+      k++;  // skip DC carrier
+      k-=frame_parms->ofdm_symbol_size;
+    }
+  }
+
+  fft((short*)syncF_tmp,          /// complex input
       (short*)primary_synch2_time,          /// complex output
       frame_parms->twiddle_ifft,    /// complex twiddle factors
       frame_parms->rev,             /// bit reversed permutation vector
@@ -107,6 +126,7 @@ int lte_sync_time_init(LTE_DL_FRAME_PARMS *frame_parms, LTE_UE_COMMON *common_va
       0);                            /// 0 - input is in complex Q1.15 format, 1 - input is in complex redundant Q1.15 format)
 
 #ifdef USER_MODE
+  write_output("primary_syncF0.m","psync0",primary_synch0_time,frame_parms->ofdm_symbol_size*2,2,1);
   write_output("primary_sync0.m","psync0",primary_synch0_time,frame_parms->ofdm_symbol_size*2,2,1);
   write_output("primary_sync1.m","psync1",primary_synch1_time,frame_parms->ofdm_symbol_size*2,2,1);
   write_output("primary_sync2.m","psync2",primary_synch2_time,frame_parms->ofdm_symbol_size*2,2,1);
@@ -132,7 +152,7 @@ short lte_sync_time(int **rxdata, ///rx data in time domain
 
   // perform a time domain correlation using the oversampled sync sequence
 
-  unsigned short n, m, ar, peak_pos;
+  unsigned int n, m, ar, peak_pos;
   int temp_re, temp_im, peak_val;
 
   if (sync_corr == NULL) {
@@ -145,6 +165,7 @@ short lte_sync_time(int **rxdata, ///rx data in time domain
 
   for (n=0; n<LTE_NUMBER_OF_SUBFRAMES_PER_FRAME*(frame_parms->samples_per_tti); n++) {
     sync_corr[n] = 0;
+    //if (n%1000==0) printf("n=%d\n",n);
     if (n<LTE_NUMBER_OF_SUBFRAMES_PER_FRAME*(frame_parms->samples_per_tti)-frame_parms->ofdm_symbol_size) {
       for (m=0; m<frame_parms->ofdm_symbol_size; m++) {
 	for (ar=0;ar<frame_parms->nb_antennas_rx;ar++) {
