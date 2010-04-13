e7ab36fbe69212a90008027f313d39f78c71b0db
latif <latif@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2010-04-13 09:51:25 +0000

    new files for running SISO capacity module from Measurements

--- openair_lte/openair1/EMOS/LTE/POST_PROCESSING/capacity_siso.m
+++ openair_lte/openair1/EMOS/LTE/POST_PROCESSING/capacity_siso.m
@@ -1,28 +1,100 @@
-function chcap_siso_single_stream = capacity_siso(h,N0)
-%Channel Capacity of SISO system with QAM modulation
+%Channel Capacity of SISO system
+clear;
+clc;
+close all;
+warning off all
+format('long');
+load 'estimates.mat';
+%M1=4;% QAM on first antenna
 
-M1=4;% QAM on first antenna
-N = 1000000;  % No of noise realizations
-%seed(1) = 13579;
-%seed(2) = 24680;
-%rand('state', seed(1));
-%randn('state', seed(2));
-sigmasq_x1=1;
-[stream1]=sqrt(sigmasq_x1)*mapping(M1);     %x1   1x648
-logsum_siso = 0;
-for s1=1:M1
-    x1=stream1(s1);
-    for k=1:N
-        z=sqrt(N0)*sqrt(1/2).*(randn(1,1)+sqrt(-1)*randn(1,1));
-        alpha_siso=h;
-        y_siso=alpha_siso*x1+z;
-        den_siso=exp((-1/N0)*norm(y_siso-alpha_siso*x1)^2);
-        neu_siso=0;
-        for g=1:M1;
-            x1_dd=stream1(g);
-            neu_siso=neu_siso+exp((-1/N0)*norm(y_siso-alpha_siso*x1_dd)^2);
+%h = zeros(1,200);
+
+chcap_siso_single_stream_4Qam_eNB1 = zeros(1,length(h));
+chcap_siso_single_stream_4Qam_eNB2 = zeros(1,length(h));
+chcap_siso_single_stream_4Qam_eNB3 = zeros(1,length(h));
+
+chcap_siso_single_stream_16Qam_eNB1 = zeros(1,length(h));
+chcap_siso_single_stream_16Qam_eNB2 = zeros(1,length(h));
+chcap_siso_single_stream_16Qam_eNB3 = zeros(1,length(h));
+
+chcap_siso_single_stream_64Qam_eNB1 = zeros(1,length(h));
+chcap_siso_single_stream_64Qam_eNB2 = zeros(1,length(h));
+chcap_siso_single_stream_64Qam_eNB3 = zeros(1,length(h));
+
+N0 = double(estimates(1).phy_measurements(1).n0_power(1));
+
+h_eNB1 = double(estimates(1).channel(1,1,1:2:end)) + 1j*double(estimates(1).channel(1,1,2:2:end));
+h_eNB2 = double(estimates(1).channel(2,1,1:2:end)) + 1j*double(estimates(1).channel(2,1,2:2:end));
+h_eNB3 = double(estimates(1).channel(3,1,1:2:end)) + 1j*double(estimates(1).channel(3,1,2:2:end));
+
+% version 1: using tabulated values
+SNR_eNB1 = 10*log10(abs(h_eNB1).^2/double(estimates(1).phy_measurements(1).n0_power(1)));
+SNR_eNB2 = 10*log10(abs(h_eNB2).^2/double(estimates(1).phy_measurements(1).n0_power(1)));
+SNR_eNB3 = 10*log10(abs(h_eNB3).^2/double(estimates(1).phy_measurements(1).n0_power(1)));
+
+for q=1:3
+    if q ==1
+        for v=1:length(h_eNB1)
+        chcap_siso_single_stream_4Qam_eNB1(v) = capacity_siso_tab_measurements(round(SNR_eNB1(v)), h_eNB1(v), N0, 4);
+        chcap_siso_single_stream_4Qam_eNB2(v) = capacity_siso_tab_measurements(round(SNR_eNB2(v)), h_eNB2(v), N0, 4);
+        chcap_siso_single_stream_4Qam_eNB3(v) = capacity_siso_tab_measurements(round(SNR_eNB3(v)), h_eNB3(v), N0, 4);
         end
-        logsum_siso=logsum_siso+log2(neu_siso/den_siso);
+    else if q==2
+            for v=1:length(h_eNB1)
+            chcap_siso_single_stream_16Qam_eNB1(v) = capacity_siso_tab_measurements(round(SNR_eNB1(v)), h_eNB1(v), N0, 16);
+            chcap_siso_single_stream_16Qam_eNB2(v) = capacity_siso_tab_measurements(round(SNR_eNB2(v)), h_eNB2(v), N0, 16);
+            chcap_siso_single_stream_16Qam_eNB3(v) = capacity_siso_tab_measurements(round(SNR_eNB3(v)), h_eNB3(v), N0, 16);
+            end
+        else
+            for v=1:length(h)
+            chcap_siso_single_stream_64Qam_eNB1(v) = capacity_siso_tab_measurements(round(SNR_eNB1(v)), h_eNB1(v), N0, 64);
+            chcap_siso_single_stream_64Qam_eNB2(v) = capacity_siso_tab_measurements(round(SNR_eNB2(v)), h_eNB2(v), N0, 64);
+            chcap_siso_single_stream_64Qam_eNB3(v) = capacity_siso_tab_measurements(round(SNR_eNB3(v)), h_eNB3(v), N0, 64);
+            end
+        end
     end
 end
-chcap_siso_single_stream=log2(M1)-logsum_siso/(M1*N);  % For SISO
+
+save 'chcap_SISO_Measurements.mat'
+% % version 2: do Monte Carlo simulation for every channel
+% N0 = double(estimates(1).phy_measurements(1).n0_power(1));
+% 
+% N = 1000;  % No of noise realizations
+% for v=1:length(h)
+%     seed(1) = 13579;
+%     seed(2) = 24680;
+%     rand('state', seed(1));
+%     randn('state', seed(2));
+%     %N0=10^(-SNR(v)/10);
+%     %P=10^(SNR(v)/10);
+%     sigmasq_x1=1;
+%     [stream1]=sqrt(sigmasq_x1)*mapping(M1);     %x1   1x648
+%     logsum_siso = 0;
+%     %h=sqrt(1/2).*(randn(1,1)+sqrt(-1)*randn(1,1));
+%     %h = estimates(1).channel(1,1,i+1)
+%     for s1=1:M1
+%         x1=stream1(s1);
+%         for k=1:N
+%             z=sqrt(N0)*sqrt(1/2).*(randn(1,1)+sqrt(-1)*randn(1,1));
+%             alpha_siso=h(v);
+%             y_siso=alpha_siso*x1+z;
+%             den_siso=exp((-1/N0)*norm(y_siso-alpha_siso*x1)^2);
+%             neu_siso=0;
+%             for g=1:M1;
+%                 x1_dd=stream1(g);
+%                 neu_siso=neu_siso+exp((-1/N0)*norm(y_siso-alpha_siso*x1_dd)^2);
+%             end
+%             logsum_siso=logsum_siso+log2(neu_siso/den_siso);
+%         end
+%     end
+%     chcap_siso_single_stream(v)=log2(M1)-logsum_siso/(M1*N);  % For SISO
+% end
+
+
+
+
+
+
+
+
+
