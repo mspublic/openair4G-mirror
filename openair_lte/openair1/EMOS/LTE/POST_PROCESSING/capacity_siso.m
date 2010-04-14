b77b045a7c2f1f0018cd6787084574a5f4e39f87
latif <latif@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2010-04-14 14:51:19 +0000

    Matlab Functions for SISO, Alamouti, and Beamforming

--- openair_lte/openair1/EMOS/LTE/POST_PROCESSING/capacity_siso.m
+++ openair_lte/openair1/EMOS/LTE/POST_PROCESSING/capacity_siso.m
@@ -1,55 +1,106 @@
 %Channel Capacity of SISO system
-clear;
-clc;
-close all;
-warning off all
+clear all;
+%clc;
+%close all;
+%warning off all
 format('long');
 load 'estimates.mat';
+load 'SISO.mat';
 %M1=4;% QAM on first antenna
 
-%h = zeros(1,200);
+h = zeros(1,200);
 
-chcap_siso_single_stream_4Qam_eNB1 = zeros(1,length(h));
-chcap_siso_single_stream_4Qam_eNB2 = zeros(1,length(h));
-chcap_siso_single_stream_4Qam_eNB3 = zeros(1,length(h));
+chcap_siso_single_stream_4Qam_eNB1 = zeros(1,length(h), length(estimates));
+chcap_siso_single_stream_4Qam_eNB2 = zeros(1,length(h), length(estimates));
+chcap_siso_single_stream_4Qam_eNB3 = zeros(1,length(h), length(estimates));
 
-chcap_siso_single_stream_16Qam_eNB1 = zeros(1,length(h));
-chcap_siso_single_stream_16Qam_eNB2 = zeros(1,length(h));
-chcap_siso_single_stream_16Qam_eNB3 = zeros(1,length(h));
+chcap_siso_single_stream_16Qam_eNB1 = zeros(1,length(h), length(estimates));
+chcap_siso_single_stream_16Qam_eNB2 = zeros(1,length(h), length(estimates));
+chcap_siso_single_stream_16Qam_eNB3 = zeros(1,length(h), length(estimates));
 
-chcap_siso_single_stream_64Qam_eNB1 = zeros(1,length(h));
-chcap_siso_single_stream_64Qam_eNB2 = zeros(1,length(h));
-chcap_siso_single_stream_64Qam_eNB3 = zeros(1,length(h));
+chcap_siso_single_stream_64Qam_eNB1 = zeros(1,length(h), length(estimates));
+chcap_siso_single_stream_64Qam_eNB2 = zeros(1,length(h), length(estimates));
+chcap_siso_single_stream_64Qam_eNB3 = zeros(1,length(h), length(estimates));
 
-N0 = double(estimates(1).phy_measurements(1).n0_power(1));
-
-h_eNB1 = double(estimates(1).channel(1,1,1:2:end)) + 1j*double(estimates(1).channel(1,1,2:2:end));
-h_eNB2 = double(estimates(1).channel(2,1,1:2:end)) + 1j*double(estimates(1).channel(2,1,2:2:end));
-h_eNB3 = double(estimates(1).channel(3,1,1:2:end)) + 1j*double(estimates(1).channel(3,1,2:2:end));
-
-% version 1: using tabulated values
-SNR_eNB1 = 10*log10(abs(h_eNB1).^2/double(estimates(1).phy_measurements(1).n0_power(1)));
-SNR_eNB2 = 10*log10(abs(h_eNB2).^2/double(estimates(1).phy_measurements(1).n0_power(1)));
-SNR_eNB3 = 10*log10(abs(h_eNB3).^2/double(estimates(1).phy_measurements(1).n0_power(1)));
-
-for q=1:3
-    if q ==1
-        for v=1:length(h_eNB1)
-        chcap_siso_single_stream_4Qam_eNB1(v) = capacity_siso_tab_measurements(round(SNR_eNB1(v)), h_eNB1(v), N0, 4);
-        chcap_siso_single_stream_4Qam_eNB2(v) = capacity_siso_tab_measurements(round(SNR_eNB2(v)), h_eNB2(v), N0, 4);
-        chcap_siso_single_stream_4Qam_eNB3(v) = capacity_siso_tab_measurements(round(SNR_eNB3(v)), h_eNB3(v), N0, 4);
-        end
-    else if q==2
-            for v=1:length(h_eNB1)
-            chcap_siso_single_stream_16Qam_eNB1(v) = capacity_siso_tab_measurements(round(SNR_eNB1(v)), h_eNB1(v), N0, 16);
-            chcap_siso_single_stream_16Qam_eNB2(v) = capacity_siso_tab_measurements(round(SNR_eNB2(v)), h_eNB2(v), N0, 16);
-            chcap_siso_single_stream_16Qam_eNB3(v) = capacity_siso_tab_measurements(round(SNR_eNB3(v)), h_eNB3(v), N0, 16);
+for est=1:length(estimates)
+    
+    N0 = double(estimates(est).phy_measurements(1).n0_power(1));
+    
+    h_eNB1 = double(estimates(est).channel(1,1,1:2:end)) + 1j*double(estimates(est).channel(1,1,2:2:end));
+    h_eNB2 = double(estimates(est).channel(2,1,1:2:end)) + 1j*double(estimates(est).channel(2,1,2:2:end));
+    h_eNB3 = double(estimates(est).channel(3,1,1:2:end)) + 1j*double(estimates(est).channel(3,1,2:2:end));
+    
+    % version 1: using tabulated values
+    SNR_eNB1 = 10*log10(abs(h_eNB1).^2/N0);
+    SNR_eNB2 = 10*log10(abs(h_eNB2).^2/N0);
+    SNR_eNB3 = 10*log10(abs(h_eNB3).^2/N0);
+    
+    while (min(SNR_eNB1) < -20)
+        [value, index]  = min(SNR_eNB1);
+        SNR_eNB1(index) = -20;
+        continue
+        
+    end
+    
+    while (max(SNR_eNB1) > 40)
+        [value, index]  = max(SNR_eNB1);
+        SNR_eNB1(index) = 40;
+        continue
+        
+    end
+    
+    while (min(SNR_eNB2) < -20)
+        [value, index]  = min(SNR_eNB2);
+        SNR_eNB2(index) = -20;
+        continue
+        
+    end
+    while (max(SNR_eNB2) > 40)
+        [value, index]  = max(SNR_eNB2);
+        SNR_eNB2(index) = 40;
+        continue
+        
+    end
+    
+    while (min(SNR_eNB3) < -20)
+        [value, index]  = min(SNR_eNB3);
+        SNR_eNB3(index) = -20;
+        continue
+        
+    end
+    while (max(SNR_eNB3) > 40)
+        [value, index]  = max(SNR_eNB3);
+        SNR_eNB3(index) = 40;
+        continue
+        
+    end
+    
+    
+    for const=1:3
+        if const ==1
+            for c=1:length(h)
+                
+                chcap_siso_single_stream_4Qam_eNB1(1, c, est) = c_siso_4Qam(find(SNR == round(SNR_eNB1(c))));
+                chcap_siso_single_stream_4Qam_eNB2(1, c, est) = c_siso_4Qam(find(SNR == round(SNR_eNB2(c))));
+                chcap_siso_single_stream_4Qam_eNB3(1, c, est) = c_siso_4Qam(find(SNR == round(SNR_eNB3(c))));
+                
             end
-        else
-            for v=1:length(h)
-            chcap_siso_single_stream_64Qam_eNB1(v) = capacity_siso_tab_measurements(round(SNR_eNB1(v)), h_eNB1(v), N0, 64);
-            chcap_siso_single_stream_64Qam_eNB2(v) = capacity_siso_tab_measurements(round(SNR_eNB2(v)), h_eNB2(v), N0, 64);
-            chcap_siso_single_stream_64Qam_eNB3(v) = capacity_siso_tab_measurements(round(SNR_eNB3(v)), h_eNB3(v), N0, 64);
+        else if const==2
+                for c=1:length(h)
+                    
+                    chcap_siso_single_stream_16Qam_eNB1(1, c, est) = c_siso_16Qam(find(SNR == round(SNR_eNB1(c))));
+                    chcap_siso_single_stream_16Qam_eNB2(1, c, est) = c_siso_16Qam(find(SNR == round(SNR_eNB2(c))));
+                    chcap_siso_single_stream_16Qam_eNB3(1, c, est) = c_siso_16Qam(find(SNR == round(SNR_eNB3(c))));
+                    
+                end
+            else
+                for c=1:length(h)
+                    
+                    chcap_siso_single_stream_64Qam_eNB1(1, c, est) = c_siso_64Qam(find(SNR == round(SNR_eNB1(c))));
+                    chcap_siso_single_stream_64Qam_eNB2(1, c, est) = c_siso_64Qam(find(SNR == round(SNR_eNB2(c))));
+                    chcap_siso_single_stream_64Qam_eNB3(1, c, est) = c_siso_64Qam(find(SNR == round(SNR_eNB3(c))));
+                    
+                end
             end
         end
     end
@@ -58,7 +109,7 @@ end
 save 'chcap_SISO_Measurements.mat'
 % % version 2: do Monte Carlo simulation for every channel
 % N0 = double(estimates(1).phy_measurements(1).n0_power(1));
-% 
+%
 % N = 1000;  % No of noise realizations
 % for v=1:length(h)
 %     seed(1) = 13579;
