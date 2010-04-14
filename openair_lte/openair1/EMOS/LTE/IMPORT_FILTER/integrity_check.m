ae9905c87e01657f5d39909cfa2becb688b4f391
latif <latif@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2010-04-14 15:08:55 +0000

    updated for not crashing

--- openair_lte/openair1/EMOS/LTE/IMPORT_FILTER/integrity_check.m
+++ openair_lte/openair1/EMOS/LTE/IMPORT_FILTER/integrity_check.m
@@ -1,20 +1,27 @@
+%%function integrity_check()
+cancel = 0;
+
 %% load file
 [filename, pathname] = uigetfile('*.EMOS', 'Pick a file');
 
 if isequal(filename,0) || isequal(pathname,0)
     disp('canceled')
-    exit
+    %exit
+    cancel = 1;
 end
-
+if cancel == 0
 if filename(10)=='1'
     is_eNb=1;
 else
     is_eNb=0;
 end
-decimation = 100;
+
+decimation = 1;
 NFrames_max = 100*60*10;
 
 [H, H_fq, estimates, gps_data, NFrames] = load_estimates_lte(fullfile(pathname, filename),NFrames_max,decimation,is_eNb);
+
+save('estimates.mat')
 
 %%
 figure(1)
@@ -31,3 +38,4 @@ title('RX_rssi_dBm')
 
 figure(3)
 plot([gps_data.longitude], [gps_data.latitude],'x');
+end
