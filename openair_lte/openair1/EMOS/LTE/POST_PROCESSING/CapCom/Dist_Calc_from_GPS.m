a008d66d6e938a1b05a75cdf6f0dce64f25e194f
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2010-07-19 11:51:11 +0000

    BUGFIX: Dist_Calc_from_GPS.m was returning miles, not km

--- openair_lte/openair1/EMOS/LTE/POST_PROCESSING/CapCom/Dist_Calc_from_GPS.m
+++ openair_lte/openair1/EMOS/LTE/POST_PROCESSING/CapCom/Dist_Calc_from_GPS.m
@@ -1,19 +1,19 @@
-%% Distance Calculator between two GPS coordinates
-%  Date:    20-04-2010
-%  Author:  Imran Latif (imran.latif@eurecom.fr)
-%  Source: http://www.mathforum.com/library/drmath/view/51711.html
-
 function [DISTANCE] = Dist_Calc_from_GPS(LAT1, LONG1, LAT2, LONG2)
-
+%% Distance Calculator between two GPS coordinates
+% [DISTANCE] = Dist_Calc_from_GPS(LAT1, LONG1, LAT2, LONG2) 
 % DISTANCE is in Kilometers. 
 % LAT1 is the latitude value of first point in decimal degrees
 % LONG1 is the longitude value of first point in decimal degrees
 % LAT2 is the latitude value of second point in decimal degrees
 % LONG2 is the longitude value of second point in decimal degrees
-
+%
 % For Example 
-
+%
 % DISTANCE = Dist_Calc_from_GPS(43.62785, 7.052418, 43.629224, 7.040884);
+%
+%  Date:    20-04-2010
+%  Author:  Imran Latif (imran.latif@eurecom.fr)
+%  Source: http://www.mathforum.com/library/drmath/view/51711.html
 
 % all converted to radians: degree/57.29577951
 
@@ -28,9 +28,9 @@ function [DISTANCE] = Dist_Calc_from_GPS(LAT1, LONG1, LAT2, LONG2)
        DISTANCE = 0; 
    else
        if ((sin(A) * sin(C) + cos(A) * cos(C) * cos(B-D)) > 1)
-           DISTANCE = 3963.1 * acos(1);                    
+           DISTANCE = 6378 * acos(1);                    
        else
-           DISTANCE = 3963.1 * acos(sin(A) * sin(C) + cos(A) * cos(C) * cos(B-D));
+           DISTANCE = 6378 * acos(sin(A) * sin(C) + cos(A) * cos(C) * cos(B-D));
        end
    end
    
