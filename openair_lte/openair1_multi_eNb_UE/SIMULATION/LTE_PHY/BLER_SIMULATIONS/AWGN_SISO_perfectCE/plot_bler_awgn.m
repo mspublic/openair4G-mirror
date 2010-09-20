e3b733d93072e2d62348a07fce091c613780820b
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2010-09-20 13:25:48 +0000

    added results for perfect CE

--- openair_lte/openair1_multi_eNb_UE/SIMULATION/LTE_PHY/BLER_SIMULATIONS/AWGN_SISO_perfectCE/plot_bler_awgn.m
+++ openair_lte/openair1_multi_eNb_UE/SIMULATION/LTE_PHY/BLER_SIMULATIONS/AWGN_SISO_perfectCE/plot_bler_awgn.m
@@ -18,35 +18,6 @@ for i=0:25
     hold on
 end
 grid on
-
-%%
-semilogy(bler0(:,1), bler0(:,2), 'b-*')
-hold on
-grid on
-semilogy(bler1(:,1), bler1(:,2),'r-*')
-semilogy(bler2(:,1), bler2(:,2),'g-*')
-semilogy(bler3(:,1), bler3(:,2), 'y-*')
-semilogy(bler4(:,1), bler4(:,2), 'k-*')
-semilogy(bler5(:,1), bler5(:,2), '--b*')
-semilogy(bler6(:,1), bler6(:,2), '--r*')
-semilogy(bler7(:,1), bler7(:,2), '--g*')
-semilogy(bler8(:,1), bler8(:,2), '--y*')
-semilogy(bler9(:,1), bler9(:,2), '--k*')
-semilogy(bler10(:,1), bler10(:,2), 'b-d')
-semilogy(bler11(:,1), bler11(:,2), 'r-d')
-semilogy(bler12(:,1), bler12(:,2), 'g-d')
-semilogy(bler13(:,1), bler13(:,2), 'y-d')
-semilogy(bler14(:,1), bler14(:,2), 'k-d')
-semilogy(bler15(:,1), bler15(:,2), '--bd')
-semilogy(bler16(:,1), bler16(:,2), '--rd')
-semilogy(bler17(:,1), bler17(:,2), 'b-s')
-semilogy(bler18(:,1), bler18(:,2), 'r-s')
-semilogy(bler19(:,1), bler19(:,2), 'g-s')
-semilogy(bler20(:,1), bler20(:,2), 'y-s')
-semilogy(bler21(:,1), bler21(:,2), 'k-s')
-semilogy(bler22(:,1), bler22(:,2), '--bs')
-semilogy(bler23(:,1), bler23(:,2), '--rs')
-semilogy(bler24(:,1), bler24(:,2), '--gs')
 legend('mcs0(QPSK)','mcs1(QPSK)','mcs2(QPSK)','mcs3(QPSK)','mcs4(QPSK)',...
        'mcs5(QPSK)','mcs6(QPSK)','mcs7(QPSK)','mcs8(QPSK)','mcs9(QPSK)',...
        'mcs10(16QAM)','mcs11(16QAM)','mcs12(16QAM)','mcs13(16QAM)',...
@@ -66,7 +37,7 @@ bps_siso = c_siso.*scale_bps;
 
 snr_all = -10:0.2:20;
 bler_all = zeros(length(snr_all),22);
-for i=0:21
+for i=0:25
     eval(sprintf('bler = bler%d;',i));
     bler_all(:,i+1) = interp1(bler(:,1), (1-bler(:,2))*get_tbs(i,25)*6*100,snr_all,'nearest','extrap');
 end
