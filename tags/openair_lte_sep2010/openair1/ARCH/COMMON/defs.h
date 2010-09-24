1f7f0e309d2b2f922a83837cf38a4adb57bb518e
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2010-09-24 07:41:08 +0000

    Undo last add (wrong revision).

--- tags/openair_lte_sep2010/openair1/ARCH/COMMON/defs.h
+++
@@ -1,6 +0,0 @@
-//#define BIGPHYS_NUMPAGES 8192
-#define BIGPHYS_NUMPAGES 19200
-
-void *bigphys_malloc(int);
-
-void reserve_mem(unsigned long buffer,unsigned long size);
--- tags/openair_lte_sep2010/openair1/PHY/CONFIG/generate_sach_seq.m
+++
@@ -1,7 +0,0 @@
-% Generate pseudo-random QPSK sequence for SACH
-
-fid=fopen("sach.txt","w");
-for i=0:7, 
-  fprintf(fid,"pilot_re[%d]: %x\npilot_im[%d]: %x\n",i,((2^16)*floor((2^16)*(rand(1,1)-.5))) + floor((2^16)*(rand(1,1)-.5)),i,((2^16)*floor((2^16)*(rand(1,1)-.5))) + floor((2^16)*(rand(1,1)-.5))); 
-end ; 
-fclose(fid)
