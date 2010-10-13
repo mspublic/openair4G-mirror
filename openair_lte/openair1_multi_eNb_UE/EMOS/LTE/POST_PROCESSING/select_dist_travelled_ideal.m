54bfd86e35aa7000c2b5ca1a169e2e07f246f19c
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2010-10-13 14:45:50 +0000

    post processing update

---
+++ openair_lte/openair1_multi_eNb_UE/EMOS/LTE/POST_PROCESSING/select_dist_travelled_ideal.m
@@ -0,0 +1,25 @@
+pathname = '/emos/EMOS/';
+mm = 'cordes';
+mode2_ideal = load(fullfile(pathname,'/Mode2/results/results_cat_UE.mat'));
+mode2 = load(fullfile(pathname,'/Mode2/results/results_UE.mat'));
+
+%%
+figure(1)
+hold off
+time = [mode2_ideal.gps_data_cat.timestamp];
+select = zeros(size(time));
+plot(time,select,'x')
+xlabel('Time')
+ylabel('Select')
+linkdata on
+
+%%
+figure(2)
+hold off
+lat = [mode2_ideal.gps_data_cat.latitude];
+lon = [mode2_ideal.gps_data_cat.longitude];
+[x,y] = plot_gps_coordinates(mm,lon,lat,[],[],'','blue','Marker','x','Line','none');
+linkdata on
+set(findobj('XDataSource','x'),'ZDataSource','select');
+
+
