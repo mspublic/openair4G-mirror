d9d9555fd40f576ca063a94cb1485a1771bdfa33
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2011-08-27 14:00:54 +0000

    Both abstraction and real PHY working. Use with -c5 from now on!

--- branches/abstraction/targets/SIMU/USER/sinr_sim.c
+++ branches/abstraction/targets/SIMU/USER/sinr_sim.c
@@ -43,7 +43,7 @@
 #define Am 20
 #define MCS_COUNT 23
 #define MCL (-70) /*minimum coupling loss (MCL) in dB*/
-double sinr[NUMBER_OF_eNB_MAX][2*25];
+//double sinr[NUMBER_OF_eNB_MAX][2*25];
 extern double sinr_bler_map[MCS_COUNT][2][9];
 
 
@@ -126,68 +126,78 @@ void calc_path_loss(node_desc_t* enb_data, node_desc_t* ue_data, channel_desc_t 
 
 
 
-void init_snr(channel_desc_t* eNB2UE, node_desc_t *enb_data, node_desc_t *ue_data, s32 UE_id,s32 eNB_id) {
+void init_snr(channel_desc_t* eNB2UE, node_desc_t *enb_data, node_desc_t *ue_data, double* sinr_dB) {
 
   int return_value;
   u16 nb_rb = 25; //No. of resource blocks
   double thermal_noise;
   int count;
       
-  //printf("Path loss for link between ue %d and enb %d is %lf and gain is %lf \n", UE_id, eNB_id, eNB2UE[UE_id][eNB_id]->path_loss_dB, gain_max); 
-  return_value = random_channel(eNB2UE);
-      
   /* Thermal noise is calculated using 10log10(K*T*B) K = Boltzmann's constant T = room temperature B = bandwidth */
-  thermal_noise = -174 + 10*log10(eNB2UE->BW); //value in dBm 
+  thermal_noise = -174 + 10*log10(eNB2UE->BW*1e6); //value in dBm 
       
-  if (0 == return_value) {
-    //freq_channel(ul_channel[UE_id][eNB_id], nb_rb);
-    freq_channel(eNB2UE, nb_rb);
+  /*
+  printf("[CHANNEL_SIM] ue %d -> enb %d: path loss %lf, thermal_noise %lf, snr %lf\n", UE_id, eNB_id, eNB2UE->path_loss_dB, thermal_noise,
+	 enb_data->tx_power_dBm 
+	 + eNB2UE->path_loss_dB
+	 - (thermal_noise + ue_data->rx_noise_level)  ); 
+  */
+
     //printf ("coupling factor is %lf\n", coupling); 
     for (count = 0; count < (2 * nb_rb); count++) {
-      sinr[eNB_id][count] = enb_data->tx_power_dBm 
+      sinr_dB[count] = enb_data->tx_power_dBm 
 	+ eNB2UE->path_loss_dB
 	- (thermal_noise + ue_data->rx_noise_level)  
 	+ 10 * log10 (pow(eNB2UE->chF[0][count].x, 2) 
 		      + pow(eNB2UE->chF[0][count].y, 2));
-      printf("Dl_link SNR for res. block %d is %lf\n", count, sinr[eNB_id][count]);
-    }
-  } 
+      //printf("Dl_link SNR for res. block %d is %lf\n", count, sinr[eNB_id][count]);
+    } 
 }//function ends
 
 
 
 
 
-void calculate_sinr(channel_desc_t* eNB2UE,double *sinr_dB, s32 UE_id, s32 eNB_id, s32 att_eNB_id) {
+void calculate_sinr(channel_desc_t* eNB2UE, node_desc_t *enb_data, node_desc_t *ue_data, double *sinr_dB) {
 
-  double interference;
+  double sir, thermal_noise;
   short nb_rb = 25; //No. of resource blocks
   short count;
-  //double interference;
+
+  /* Thermal noise is calculated using 10log10(K*T*B) K = Boltzmann's constant T = room temperature B = bandwidth */
+  thermal_noise = -174 + 10*log10(eNB2UE->BW*1e6); //value in dBm 
 
   for (count = 0; count < 2 * nb_rb; count++) {
-    interference = 0;
-    sinr_dB[count]=sinr[att_eNB_id][count];
-    for (eNB_id = 0; eNB_id < NB_eNB_INST; eNB_id++) {
-      if (att_eNB_id != eNB_id) {
-        interference += pow(10, 0.1 * sinr[eNB_id][count]);
-      }
-    }
-    sinr_dB[count] -= 10*log10(1 + interference);
+    sir = enb_data->tx_power_dBm 
+      + eNB2UE->path_loss_dB
+      - (thermal_noise + ue_data->rx_noise_level)  
+      + 10 * log10 (pow(eNB2UE->chF[0][count].x, 2) 
+		    + pow(eNB2UE->chF[0][count].y, 2));
+    if (sir > 0)
+      sinr_dB[count] -= sir;
     //printf("*****sinr% lf \n",sinr_dB[count]);
   }
 }
+
 void get_beta_map() {
+  char *file_path = NULL;
+  /*
   char *file_name[] = {"bler_1.csv", "bler_2.csv", "bler_3.csv", "bler_4.csv", "bler_5.csv", "bler_6.csv", "bler_7.csv", "bler_8.csv",
                     "bler_9.csv", "bler_10.csv", "bler_11.csv", "bler_12.csv", "bler_13.csv", "bler_14.csv", "bler_15.csv", "bler_16.csv",
                     "bler_17.csv", "bler_18.csv", "bler_19.csv", "bler_20.csv", "bler_21.csv", "bler_22.csv"};
-  
+  */
   int table_len = 0;
   int mcs = 0;
   char *sinr_bler;
   char buffer[100];
   FILE *fp;
- 
+
+  /*
+  file_path_ptr = strcat(file_path_ptr,getenv("OPENAIR1_DIR"));
+  file_path_ptr = strcat(file_path_ptr,"/SIMULATION/LTE_PHY/Abstraction");
+  */
+  file_path = (char*) malloc(256);
+
   for (mcs = 0; mcs <= 4; mcs++) {
     for (table_len = 0; table_len < 9; table_len++) {
       sinr_bler_map[mcs][0][table_len] = 0.0;
@@ -196,9 +206,11 @@ void get_beta_map() {
   }
 
   for (mcs = 5; mcs < MCS_COUNT; mcs++) {
-    fp = fopen(file_name[mcs - 1],"r");
+    sprintf(file_path,"%s/SIMULATION/LTE_PHY/Abstraction/bler_%d.csv",getenv("OPENAIR1_DIR"),mcs);
+    fp = fopen(file_path,"r");
     if (fp == NULL) {
-      printf("ERROR: Unable to open the file %s\n", file_name[mcs-1]);
+      printf("ERROR: Unable to open the file %s\n", file_path);
+      exit(-1);
     }
     else {
       fgets(buffer, 100, fp);
@@ -217,6 +229,7 @@ void get_beta_map() {
     for (table_len = 0; table_len < 9; table_len++)
       printf("%lf  %lf \n ",sinr_bler_map[mcs][0][table_len],sinr_bler_map[mcs][1][table_len]);
   }
+  free(file_path);
 }
 
 
