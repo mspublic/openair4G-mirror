749cc80b64ed55f9713c14111965f69761046921
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2011-10-05 10:19:12 +0000

    protected full buffer model with ifdef, since with the current greedy scheduler it does not work for more than 1 UE

--- trunk/openair2/LAYER2/MAC/eNB_scheduler.c
+++ trunk/openair2/LAYER2/MAC/eNB_scheduler.c
@@ -1803,10 +1803,14 @@ void schedule_ue_spec(unsigned char Mod_id,unsigned char subframe,u16 nb_rb_used
 
 				// Now compute number of required RBs for total sdu length
 				// Assume RAH format 2
+#ifdef FULL_BUFFER
 				nb_rb = nb_available_rb;
+#else
+				nb_rb = 2;
+#endif
 				TBS = mac_xface->get_TBS(eNB_UE_stats->DL_cqi[0],nb_rb);
 
-				/*
+#ifndef FULL_BUFFER
 				while (TBS < (sdu_length_total + offset))  {
 					nb_rb += 2;  // to be replaced with RA allocation size for other than 25 PRBs!!!!!!!
 					if (nb_rb>mac_xface->lte_frame_parms->N_RB_DL) { // if we've gone beyond the maximum number of RBs
@@ -1817,7 +1821,7 @@ void schedule_ue_spec(unsigned char Mod_id,unsigned char subframe,u16 nb_rb_used
 					}
 					TBS = mac_xface->get_TBS(eNB_UE_stats->DL_cqi[0],nb_rb);
 				}
-				*/
+#endif
 
 #ifdef DEBUG_eNB_SCHEDULER
 				msg("[MAC][eNB %d] Generated DLSCH header (mcs %d, TBS %d, nb_rb %d)\n",
@@ -1851,6 +1855,7 @@ void schedule_ue_spec(unsigned char Mod_id,unsigned char subframe,u16 nb_rb_used
 				case 1:
 				case 2:
 				default:
+				  // TODO: set all fields here 
 					((DCI1_5MHz_TDD_t*)DLSCH_dci)->mcs = eNB_UE_stats->DL_cqi[0];
 					((DCI1_5MHz_TDD_t*)DLSCH_dci)->harq_pid = harq_pid;
 					((DCI1_5MHz_TDD_t*)DLSCH_dci)->ndi = 1;
