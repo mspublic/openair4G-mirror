d2b9013cf85306e959b6be48eee3b38b0b1d6ae4
demiray <demiray@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2011-12-07 14:31:29 +0000

    * Test code updated for pdcp_data_ind() * PDCP code updated according to new log generator * readme.txt and todo.txt under PDCP test directory updated

--- targets/TEST/PDCP/test_util.h
+++ targets/TEST/PDCP/test_util.h
@@ -8,6 +8,8 @@
 #ifndef TEST_UTIL_H
 #define TEST_UTIL_H
 
+#include "LAYER2/PDCP_v10.1.0/pdcp.h"
+
 /*
  * Prints binary representation of given octet prepending 
  * passed log message
@@ -28,6 +30,29 @@ void print_binary_representation(unsigned char* message, unsigned char byte)
     mask /= 2;
   }
   printf("\n");
+}
+
+/*
+ * Prints PDCP properties
+ *
+ * @param pdcp_t pointer for relevant PDCP entity
+ * @return none
+ */
+void print_pdcp_properties(pdcp_t* pdcp_entity)
+{
+  printf("PDCP properties:\n");
+
+  if (pdcp_entity == NULL) {
+    printf("PDCP pointer is invalid!\n");
+  } else {
+    printf("-> Is header compression active? %s\n", ((pdcp_entity->header_compression_active == TRUE) ? "Yes" : "No"));
+    printf("-> Sequence number size: %d bits\n", pdcp_entity->seq_num_size);
+    printf("-> Next TX sequence number: %d\n", pdcp_entity->next_pdcp_tx_sn);
+    printf("-> Next RX sequence number: %d\n", pdcp_entity->next_pdcp_rx_sn);
+    printf("-> TX HFN: %d\n", pdcp_entity->tx_hfn);
+    printf("-> RX HFN: %d\n", pdcp_entity->rx_hfn);
+    printf("-> Last submitted RX sequence number: %d\n", pdcp_entity->last_submitted_pdcp_rx_sn);
+  }
 }
 
 #endif
