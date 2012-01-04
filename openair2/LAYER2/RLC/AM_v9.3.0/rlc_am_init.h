8b9370d70d4d326a67ed06fb0db7d70acea3c598
gauthier <gauthier@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-01-04 15:25:02 +0000

    Updated doc, polishing

--- openair2/LAYER2/RLC/AM_v9.3.0/rlc_am_init.h
+++ openair2/LAYER2/RLC/AM_v9.3.0/rlc_am_init.h
@@ -31,11 +31,13 @@ Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis
 * \author GAUTHIER Lionel
 * \date 2010-2011
 * \version
-* \company Eurecom
-* \email: lionel.gauthier@eurecom.fr
 * \note
 * \bug
 * \warning
+*/
+/** @defgroup _rlc_am_init_impl_ RLC AM Init Reference Implementation
+* @ingroup _rlc_am_impl_
+* @{
 */
 #ifndef __RLC_AM_INIT_H__
 #    define __RLC_AM_INIT_H__
@@ -62,9 +64,43 @@ Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis
 #include "platform_constants.h"
 #include "PHY/defs.h"
 
+
+/*! \struct  rlc_am_info_t
+* \brief Structure containing RLC AM configuration parameters.
+*/
+typedef volatile struct {
+    u16_t max_retx_threshold;  /*!< \brief Maximum number of retransmissions for one RLC AM PDU. */
+    u16_t poll_pdu;            /*!< \brief Generate a status each poll_pdu pdu sent. */
+    u16_t poll_byte;           /*!< \brief Generate a status each time poll_byte bytes have been sent. */
+    u32_t t_poll_retransmit;   /*!< \brief t-PollRetransmit timer initial value. */
+    u32_t t_reordering;        /*!< \brief t-Reordering timer initial value. */
+    u32_t t_status_prohibit;   /*!< \brief t-StatusProhibit timer initial value. */
+} rlc_am_info_t;
+
 //-----------------------------------------------------------------------------
+/*! \fn void rlc_am_init   (rlc_am_entity_t* rlcP)
+* \brief    Initialize the RLC AM protocol instance, reset variables, allocate buffers, lists, then, the next step in order have a running RLC AM instance is to configure and set debug informations for this RLC instance.
+* \param[in]  rlcP                      RLC AM protocol instance pointer.
+*/
 public_rlc_am_init( void rlc_am_init   (rlc_am_entity_t* rlcP);)
+
+/*! \fn void rlc_am_cleanup(rlc_am_entity_t* rlcP)
+* \brief    Free all memory resources allocated and kept by this RLC AM instance.
+* \param[in]  rlcP                      RLC AM protocol instance pointer.
+*/
 public_rlc_am_init( void rlc_am_cleanup(rlc_am_entity_t* rlcP);)
+
+/*! \fn void rlc_am_configure(rlc_am_entity_t *rlcP, u16_t max_retx_thresholdP, u16_t poll_pduP, u16_t poll_byteP, u32_t t_poll_retransmitP, u32_t t_reorderingP, u32_t t_status_prohibitP)
+* \brief    Set RLC AM protocol parameters.
+* \param[in]  rlcP                      RLC AM protocol instance pointer.
+* \param[in]  max_retx_thresholdP       Limit the number of retransmissions of an
+AMD PDU.
+* \param[in]  poll_pduP                 Trigger a poll for every poll_pduP PDUs.
+* \param[in]  poll_byteP                Trigger a poll for every pollByte bytes.
+* \param[in]  t_poll_retransmitP        This timer is used by the transmitting side of an AM RLC entity in order to retransmit a poll, value in frames.
+* \param[in]  t_reorderingP             This timer is used by the receiving side of an AM RLC entity in order to detect loss of RLC PDUs at lower layer, value in frames.
+* \param[in]  t_status_prohibitP        This timer is used by the receiving side of an AM RLC entity in order to prohibit transmission of a STATUS PDU, value in frames.
+*/
 public_rlc_am_init( void rlc_am_configure(rlc_am_entity_t *rlcP,
                                           u16_t max_retx_thresholdP,
                                           u16_t poll_pduP,
@@ -72,5 +108,14 @@ public_rlc_am_init( void rlc_am_configure(rlc_am_entity_t *rlcP,
                                           u32_t t_poll_retransmitP,
                                           u32_t t_reorderingP,
                                           u32_t t_status_prohibitP);)
+
+/*! \fn void rlc_am_set_debug_infos(rlc_am_entity_t *rlcP, module_id_t module_idP, rb_id_t rb_idP, rb_type_t rb_typeP)
+* \brief    Set informations that will be displayed in traces, helping the debug process.
+* \param[in]  rlcP                      RLC AM protocol instance pointer.
+* \param[in]  module_idP                Virtualization variable, module identifier.
+* \param[in]  rb_idP                    Radio bearer identifier.
+* \param[in]  rb_typeP                  Radio bearer type (Signalling or data).
+*/
 public_rlc_am_init( void rlc_am_set_debug_infos(rlc_am_entity_t *rlcP, module_id_t module_idP, rb_id_t rb_idP, rb_type_t rb_typeP);)
+/** @} */
 #endif
