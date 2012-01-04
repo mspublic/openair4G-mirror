57d5e1ad2a2392aab65d0fc9319c26927880805e
gauthier <gauthier@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-01-04 15:23:55 +0000

    Updated doc, polishing

--- trunk/openair2/LAYER2/RLC/UM_v9.3.0/rlc_um_constants.h
+++ trunk/openair2/LAYER2/RLC/UM_v9.3.0/rlc_um_constants.h
@@ -31,20 +31,28 @@ Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis
 * \author GAUTHIER Lionel
 * \date 2010-2011
 * \version
-* \company Eurecom
-* \email: lionel.gauthier@eurecom.fr
 * \note
 * \bug
 * \warning
 */
+/**
+* @addtogroup _rlc_um_internal_impl_
+* @{
+*/
 #    ifndef __RLC_UM_CONSTANTS_H__
 #        define __RLC_UM_CONSTANTS_H__
 
+/** The sequence numbering modulo (10 bits). */
 #        define RLC_UM_SN_10_BITS_MODULO                    1024
+/** The sequence numbering binary mask (10 bits). */
 #        define RLC_UM_SN_10_BITS_MASK                      0x03FF
+/** UM_Window_Size = 512 when a 10 bit SN is configured. */
 #        define RLC_UM_WINDOW_SIZE_SN_10_BITS               512
+/** The sequence numbering modulo (5 bits). */
 #        define RLC_UM_SN_5_BITS_MODULO                     32
+/** The sequence numbering binary mask (5 bits). */
 #        define RLC_UM_SN_5_BITS_MASK                       0x1F
+/** UM_Window_Size = 16 when a 5 bit SN is configured. */
 #        define RLC_UM_WINDOW_SIZE_SN_5_BITS                16
          // This constant is used by the receiving UM RLC entity to define SNs of those
          // UMD PDUs that can be received without causing an advancement of the
@@ -55,9 +63,19 @@ Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis
 #        define RLC_UM_SEGMENT_NB_MAX_LI_PER_PDU            24
 //----------------------------------------------------------
 // Events defined for state model of the acknowledged mode entity
+/** Internal event defined for state model of the RLC UM. */
 #        define RLC_UM_RECEIVE_CRLC_CONFIG_REQ_ENTER_NULL_STATE_EVENT                 0x00
+
+/** Internal event defined for state model of the RLC UM. */
 #        define RLC_UM_RECEIVE_CRLC_CONFIG_REQ_ENTER_DATA_TRANSFER_READY_STATE_EVENT  0x01
+
+/** Internal event defined for state model of the RLC UM. */
 #        define RLC_UM_RECEIVE_CRLC_SUSPEND_REQ_EVENT                                 0x10
+
+/** Internal event defined for state model of the RLC UM. */
 #        define RLC_UM_TRANSMIT_CRLC_SUSPEND_CNF_EVENT                                0x11
+
+/** Internal event defined for state model of the RLC UM. */
 #        define RLC_UM_RECEIVE_CRLC_RESUME_REQ_EVENT                                  0x12
+/** @} */
 #    endif
--- trunk/openair2/LAYER2/RLC/UM_v9.3.0/rlc_um_dar.h
+++ trunk/openair2/LAYER2/RLC/UM_v9.3.0/rlc_um_dar.h
@@ -31,11 +31,13 @@ Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis
 * \author GAUTHIER Lionel
 * \date 2010-2011
 * \version
-* \company Eurecom
-* \email: lionel.gauthier@eurecom.fr
 * \note
 * \bug
 * \warning
+*/
+/** @defgroup _rlc_um_receiver_impl_ RLC UM Receiver Implementation
+* @ingroup _rlc_um_impl_
+* @{
 */
 #    ifndef __RLC_UM_DAR_H__
 #        define __RLC_UM_DAR_H__
@@ -60,12 +62,80 @@ Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis
 #                define public_rlc_um_dar(x)     extern x
 #            endif
 #        endif
+/*! \fn signed int rlc_um_get_pdu_infos(rlc_um_pdu_sn_10_t* headerP, s16_t total_sizeP, rlc_um_pdu_info_t* pdu_infoP)
+* \brief    Extract PDU informations (header fields, data size, etc) from the serialized PDU.
+* \param[in]  headerP             RLC UM header PDU pointer.
+* \param[in]  total_sizeP         Size of RLC UM PDU.
+* \param[in]  pdu_infoP           Structure containing extracted informations from PDU.
+* \return     0 if no error was encountered during the parsing of the PDU, else -1;
+*/
 private_rlc_um_dar(  signed int rlc_um_get_pdu_infos(rlc_um_pdu_sn_10_t* headerP, s16_t total_sizeP, rlc_um_pdu_info_t* pdu_infoP));
+
+/*! \fn int rlc_um_read_length_indicators(unsigned char**dataP, rlc_um_e_li_t* e_liP, unsigned int* li_arrayP, unsigned int *num_liP, unsigned int *data_sizeP)
+* \brief    Reset protocol variables and state variables to initial values.
+* \param[in,out]  dataP          Pointer on data payload.
+* \param[in]      e_liP          Pointer on first LI + e bit in PDU.
+* \param[in,out]  li_arrayP      Array containing read LI.
+* \param[in,out]  num_liP        Number of LI read.
+* \param[in,out]  data_sizeP     Pointer on data size.
+* \return     0 if no error was encountered during the parsing of the PDU, else -1;
+*/
 private_rlc_um_dar(  int rlc_um_read_length_indicators(unsigned char**dataP, rlc_um_e_li_t* e_liP, unsigned int* li_arrayP, unsigned int *num_liP, unsigned int *data_sizeP));
+
+/*! \fn void rlc_um_try_reassembly      (rlc_um_entity_t *rlcP, signed int snP)
+* \brief    Try reassembly PDUs from DAR buffer, starting at sequence number snP.
+* \param[in]  rlcP        RLC UM protocol instance pointer.
+* \param[in]  snP         Sequence number.
+*/
 private_rlc_um_dar(  void rlc_um_try_reassembly      (rlc_um_entity_t *rlcP, signed int snP));
+
+/*! \fn void rlc_um_check_timer_dar_time_out(rlc_um_entity_t *rlcP)
+* \brief    Check if t-Reordering expires and take the appropriate actions as described in 3GPP specifications.
+* \param[in]  rlcP        RLC UM protocol instance pointer.
+*/
 private_rlc_um_dar(  void rlc_um_check_timer_dar_time_out(rlc_um_entity_t *rlcP));
+
+/*! \fn mem_block_t *rlc_um_remove_pdu_from_dar_buffer(rlc_um_entity_t *rlcP, u16_t snP)
+* \brief    Remove the PDU with sequence number snP from the DAR buffer and return it.
+* \param[in]  rlcP        RLC UM protocol instance pointer.
+* \param[in]  snP         Sequence number.
+* \return     The PDU stored in the DAR buffer having sequence number snP, else return NULL.
+*/
 private_rlc_um_dar(  mem_block_t *rlc_um_remove_pdu_from_dar_buffer(rlc_um_entity_t *rlcP, u16_t snP));
+
+/*! \fn mem_block_t *rlc_um_remove_pdu_from_dar_buffer(rlc_um_entity_t *rlcP, u16_t snP)
+* \brief    Get the PDU with sequence number snP from the DAR buffer.
+* \param[in]  rlcP        RLC UM protocol instance pointer.
+* \param[in]  snP         Sequence number.
+* \return     The PDU stored in the DAR buffer having sequence number snP, else return NULL.
+*/
 private_rlc_um_dar(  inline mem_block_t* rlc_um_get_pdu_from_dar_buffer(rlc_um_entity_t *rlcP, u16_t snP));
+
+/*! \fn signed int rlc_um_in_window(rlc_um_entity_t *rlcP, signed int lower_boundP, signed int snP, signed int higher_boundP)
+* \brief    Compute if the sequence number of a PDU is in a window .
+* \param[in]  rlcP           RLC UM protocol instance pointer.
+* \param[in]  lower_boundP   Lower bound of a window.
+* \param[in]  snP            Sequence number of a theorical PDU.
+* \param[in]  higher_boundP  Higher bound of a window.
+* \return     -2 if lower_boundP  > sn, -1 if higher_boundP < sn, 0 if lower_boundP  < sn < higher_boundP, 1 if lower_boundP  == sn, 2 if higher_boundP == sn, 3 if higher_boundP == sn == lower_boundP.
+*/
 protected_rlc_um_dar(inline signed int rlc_um_in_window(rlc_um_entity_t *rlcP, signed int lower_boundP, signed int snP, signed int higher_boundP));
+
+/*! \fn signed int rlc_um_in_reordering_window(rlc_um_entity_t *rlcP, signed int snP)
+* \brief    Compute if the sequence number of a PDU is in a window .
+* \param[in]  rlcP           RLC UM protocol instance pointer.
+* \param[in]  snP            Sequence number of a theorical PDU.
+* \return     0 if snP is in reordering window, else -1.
+*/
+protected_rlc_um_dar(inline signed int rlc_um_in_reordering_window(rlc_um_entity_t *rlcP, signed int snP));
+
+/*! \fn void rlc_um_receive_process_dar (rlc_um_entity_t *rlcP, mem_block_t *pdu_memP,rlc_um_pdu_sn_10_t *pduP, u16_t tb_sizeP)
+* \brief    Apply the DAR process for a PDU: put it in DAR buffer and try to reassembly or discard it.
+* \param[in]  rlcP       RLC UM protocol instance pointer.
+* \param[in]  pdu_memP   mem_block_t wrapper for a UM PDU .
+* \param[in]  pduP       Pointer on the header of the UM PDU.
+* \param[in]  tb_sizeP   Size of the UM PDU.
+*/
 protected_rlc_um_dar(void rlc_um_receive_process_dar (rlc_um_entity_t *rlcP, mem_block_t *pdu_memP,rlc_um_pdu_sn_10_t *pduP, u16_t tb_sizeP));
+/** @} */
 #    endif
