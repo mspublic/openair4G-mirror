67a4e9efce38b552f9ddc31f6f47d531f265f7dc
bhamri <bhamri@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-03-01 07:24:55 +0000

    added files for new forms in tags TM5

---
+++ tags/TM5-Dec2011/targets/SIMU/USER/MUMIMO_form.h
@@ -0,0 +1,21 @@
+
+#ifndef FD_MUMIMO_h_
+#define FD_MUMIMO_h
+
+typedef struct{
+  FL_FORM *MUMIMO;
+  void *vdata;
+  char *cdata;
+  long  ldata;
+  FL_OBJECT *subband;
+  FL_OBJECT *piechart;
+  FL_OBJECT *plot_avg;
+  FL_OBJECT *plot_instant_SU;
+  FL_OBJECT *plot_instant_MU;
+  FL_OBJECT *plot_instant_FMU;
+} FD_MUMIMO;
+
+
+extern FD_MUMIMO * create_form_MUMIMO(int);
+
+#endif
