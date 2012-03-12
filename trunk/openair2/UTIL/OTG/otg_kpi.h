94458b4f22ed89f6e0dfb063fe180e94cb21250b
hafsaoui <hafsaoui@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-03-12 10:24:02 +0000

    KPI computation module

---
+++ trunk/openair2/UTIL/OTG/otg_kpi.h
@@ -0,0 +1,123 @@
+/*******************************************************************************
+
+  Eurecom OpenAirInterface
+  Copyright(c) 1999 - 2011 Eurecom
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+  Contact Information
+  Openair Admin: openair_admin@eurecom.fr
+  Openair Tech : openair_tech@eurecom.fr
+  Forums       : http://forums.eurecom.fsr/openairinterface
+  Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis, France
+
+*******************************************************************************/
+
+/*! \file otg_kpi.h functions to compute OTG KPIs
+* \brief desribe function for KPIs computation 
+* \author A. Hafsaoui
+* \date 2012
+* \version 0.1
+* \company Eurecom
+* \email: openair_tech@eurecom.fr
+* \note
+* \warning
+
+*/
+
+#ifndef __OTG_KPI_H__
+#	define __OTG_KPI_H__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "otg.h"
+#include "otg_defs.h"
+#include "otg_externs.h"
+#include "otg_config.h"
+
+
+
+#ifndef __LOG_H__
+	#define LOG_G(c, x...) printf(x)
+	#define LOG_A(c, x...) printf(x)
+	#define LOG_C(c, x...) printf(x)
+	#define LOG_E(c, x...) printf(x)
+	#define LOG_W(c, x...) printf(x)
+	#define LOG_N(c, x...) printf(x)
+	#define LOG_I(c, x...) printf(x)
+	#define LOG_D(c, x...) printf(x)
+	#define LOG_F(c, x...) printf(x)  
+	#define LOG_T(c, x...) printf(x)
+	#include "otg_vars.h"
+#else
+	#include "/UTIL/LOG/log.h"
+	#include "otg_externs.h"
+#endif 
+
+
+
+
+
+/*! \fn void tx_throughput( int src, int dst)
+* \brief compute the transmitter throughput in bytes per seconds
+* \param[in] Source, destination
+* \param[out]
+* \note 
+* @ingroup  _otg
+*/
+void tx_throughput( int src, int dst);
+
+/*! \fn void rx_goodput( int src, int dst)
+* \brief compute the receiver goodput in bytes per seconds
+* \param[in] Source, destination
+* \param[out] 
+* \note 
+* @ingroup  _otg
+*/
+void rx_goodput( int src, int dst);
+
+
+/*void rx_loss_rate_pkts(int src, int dst)
+* \brief compute the loss rate in bytes at the server bytes
+* \param[in] Source, destination
+* \param[out] 
+* \note 
+* @ingroup  _otg
+*/
+void rx_loss_rate_pkts(int src, int dst);
+
+/*void rx_loss_rate_bytes(int src, int dst)
+* \brief compute the loss rate in pkts at the server bytes
+* \param[in] Source, destination
+* \param[out] 
+* \note 
+* @ingroup  _otg
+*/
+void rx_loss_rate_bytes(int src, int dst);
+
+/*void kpi_gen(void)
+* \brief compute KPIs after the end of the simulation 
+* \param[in] 
+* \param[out] 
+* \note 
+* @ingroup  _otg
+*/
+void kpi_gen(void);
+
+
+#endif
