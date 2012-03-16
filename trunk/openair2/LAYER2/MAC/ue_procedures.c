bfba3b17e5d193a23b3821e459240d5192c7a7dd
wetterwa <wetterwa@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-03-16 08:53:50 +0000

    RRC Cellular update - MW

--- trunk/openair2/LAYER2/MAC/ue_procedures.c
+++ trunk/openair2/LAYER2/MAC/ue_procedures.c
@@ -778,7 +778,9 @@ UE_L2_STATE_t ue_scheduler(u8 Mod_id,u32 frame, u8 subframe, lte_subframe_t dire
   //Mac_rlc_xface->frame=frame;
   //Rrc_xface->Frame_index=Mac_rlc_xface->frame;
   pdcp_run(frame, 0);
-
+#ifdef CELLULAR
+  Rrc_xface->rrc_rx_tx(Mod_id, frame, 0, eNB_index);
+#else
   switch (rrc_rx_tx(Mod_id,
 		    frame,
 		    0,
@@ -792,6 +794,7 @@ UE_L2_STATE_t ue_scheduler(u8 Mod_id,u32 frame, u8 subframe, lte_subframe_t dire
   default:
     break;
   }
+#endif 
 
   // Check timers
   if (UE_mac_inst[Mod_id].RA_contention_resolution_timer_active == 1) {
