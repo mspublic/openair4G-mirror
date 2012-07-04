43667b966d619f3ec340f74c6ff68cf56cc3173c
demiray <demiray@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-07-04 17:16:26 +0000

    v1.0.16 * Openair source file template has been prepended to every source file * README.HEADER_TEMPLATE file has been added * 'CM-GN Interface Definition_v4.pdf' and   'ITS Station Management Core - Low Level Description.pdf' files has been   added under Documentation/ and Documentation/README file has been updated * Doxygen documentation has been added for mgmt_its_key_manager.hpp file

--- trunk/openair3/SCOREF-MGMT/Software/src/packets/mgmt_gn_packet_wireless_state_response.cpp
+++ trunk/openair3/SCOREF-MGMT/Software/src/packets/mgmt_gn_packet_wireless_state_response.cpp
@@ -1,9 +1,43 @@
-/*
- * mgmt_gn_packet_wireless_state.cpp
- *
- *  Created on: May 11, 2012
- *      Author: demiray
- */
+/*******************************************************************************
+
+  Eurecom OpenAirInterface
+  Copyright(c) 1999 - 2012 Eurecom
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+  Contact Information
+  Openair Admin: openair_admin@eurecom.fr
+  Openair Tech : openair_tech@eurecom.fr
+  Forums       : http://forums.eurecom.fsr/openairinterface
+  Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis, France
+
+*******************************************************************************/
+
+/*!
+ * \file mgmt_gn_packet_wireless_state.cpp
+ * \brief A container for Wireless State Event Response packet
+ * \company EURECOM
+ * \date 2012
+ * \author Baris Demiray
+ * \email: baris.demiray@eurecom.fr
+ * \note none
+ * \bug none
+ * \warning none
+*/
 
 #include "mgmt_gn_packet_wireless_state_response.hpp"
 #include "../util/mgmt_util.hpp"
