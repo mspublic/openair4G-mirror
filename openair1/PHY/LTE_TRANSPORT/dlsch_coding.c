d5085e6d78bea67c4e33eaf64c5ba86f4b02feba
wagner <wagner@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-07-19 15:16:36 +0000

    - bugfix: disabled memory allocation for "dlsch->harq_processes[i]->b"

--- openair1/PHY/LTE_TRANSPORT/dlsch_coding.c
+++ openair1/PHY/LTE_TRANSPORT/dlsch_coding.c
@@ -73,12 +73,12 @@ void free_eNB_dlsch(LTE_eNB_DLSCH_t *dlsch) {
 #ifdef DEBUG_DLSCH_FREE
 	msg("Freeing dlsch process %d (%p)\n",i,dlsch->harq_processes[i]);
 #endif
-	if (dlsch->harq_processes[i]->b) {
+	/*if (dlsch->harq_processes[i]->b) {
 	  free16(dlsch->harq_processes[i]->b,MAX_DLSCH_PAYLOAD_BYTES);
 #ifdef DEBUG_DLSCH_FREE
 	  msg("Freeing dlsch process %d b (%p)\n",i,dlsch->harq_processes[i]->b);
 #endif
-	}
+}*/
 	if (dlsch->harq_processes[i]->c) {
 #ifdef DEBUG_DLSCH_FREE
 	  msg("Freeing dlsch process %d c (%p)\n",i,dlsch->harq_processes[i]->c);
@@ -118,11 +118,11 @@ LTE_eNB_DLSCH_t *new_eNB_dlsch(unsigned char Kmimo,unsigned char Mdlharq,u8 abst
       //printf("dlsch->harq_processes[%d] %p\n",i,dlsch->harq_processes[i]);
       if (dlsch->harq_processes[i]) {
 	bzero(dlsch->harq_processes[i],sizeof(LTE_DL_eNB_HARQ_t));
-	dlsch->harq_processes[i]->b          = (unsigned char*)malloc16(MAX_DLSCH_PAYLOAD_BYTES);
+    /*	dlsch->harq_processes[i]->b          = (unsigned char*)malloc16(MAX_DLSCH_PAYLOAD_BYTES);
 	if (!dlsch->harq_processes[i]->b) {
 	  msg("Can't get b\n");
 	  exit_flag=1;
-	}
+      }*/
 	if (abstraction_flag==0) {
 	  for (r=0;r<MAX_NUM_DLSCH_SEGMENTS;r++) {
 	    dlsch->harq_processes[i]->c[r] = (unsigned char*)malloc16(((r==0)?8:0) + 3+(MAX_DLSCH_PAYLOAD_BYTES));  // account for filler in first segment and CRCs for multiple segment case
