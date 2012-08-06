e4c7499bdb11c87a265ddfb121f6f95298097937
agostini <agostini@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2012-08-06 09:48:10 +0000

    MAJ ieee80211p

--- trunk/openairITS/phy/DRIVERS/ieee80211p_start.sh
+++ trunk/openairITS/phy/DRIVERS/ieee80211p_start.sh
@@ -13,19 +13,22 @@ cp /home/thales/openair4G/openairITS/openair2/ieee80211p/compat/compat.ko /lib/m
 cp /home/thales/openair4G/openairITS/openair2/ieee80211p/net/wireless/cfg80211.ko /lib/modules/2.6.32.11+drm33.2.openairinterface.bigphys.rtai/updates/net/wireless/cfg80211.ko
 cp /home/thales/openair4G/openairITS/openair2/ieee80211p/net/mac80211/mac80211_eurecom.ko /lib/modules/2.6.32.11+drm33.2.openairinterface.bigphys.rtai/updates/net/mac80211/mac80211_eurecom.ko
 
-cd /home/thales/openair4G/openairITS/openair1/DRIVERS/
+cd /home/thales/openair4G/openairITS/phy/DRIVERS/
 make clean
 make
 
 # Module loading
 modprobe mac80211_eurecom
-insmod /home/thales/openair4G/openairITS/openair1/DRIVERS/ieee80211p.ko
+insmod /home/thales/openair4G/openairITS/phy/DRIVERS/ieee80211p.ko
 
-# Interface configuration
+# Interface configuration (interface type, MAC address, IP address, disable ARP)
 iw phy phy0 interface add wlan0 type ibss 4addr off
-#iw dev wlan0 dev set type ibss
-ifconfig wlan0 192.168.1.2 up -arp
+ifconfig wlan0 hw ether 10:11:12:13:14:15
+ifconfig wlan0 192.168.1.1 up -arp
 
 # Static ARP table
-arp -i wlan0 -s 192.168.1.2 10:11:12:13:14:15
+arp -i wlan0 -s 192.168.1.2 10:21:22:23:24:25
 arp -i wlan0 -s 192.168.1.255 FF:FF:FF:FF:FF:FF
+
+# Softmodem interface software test
+gcc -W -Wall -o ./ieee80211p-softmodem ./ieee80211p-softmodem.c ./ieee80211p-netlinkapi.c
