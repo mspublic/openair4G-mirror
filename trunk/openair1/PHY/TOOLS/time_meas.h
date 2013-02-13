c13f193482f7a3c253c3a9fdd0e892c2ee01d81e
wagner <wagner@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-02-13 09:05:12 +0000

    - renamed function rdtsc since the name was used by RTAI

--- trunk/openair1/PHY/TOOLS/time_meas.h
+++ trunk/openair1/PHY/TOOLS/time_meas.h
@@ -10,16 +10,15 @@ static inline void start_meas(time_stats_t *ts) __attribute__((always_inline));
 static inline void stop_meas(time_stats_t *ts) __attribute__((always_inline));
 
 #if defined(__i386__)
-static inline unsigned long long rdtsc(void) __attribute__((always_inline));
-static inline unsigned long long rdtsc(void)
-{
-  unsigned long long int x;
-     __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
-     return x;
+static inline unsigned long long rdtsc_oai(void) __attribute__((always_inline));
+static inline unsigned long long rdtsc_oai(void) {
+    unsigned long long int x;
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
 }
 #elif defined(__x86_64__)
-static inline unsigned long long rdtsc() __attribute__((always_inline));
-static inline unsigned long long rdtsc() { 
+static inline unsigned long long rdtsc_oai() __attribute__((always_inline));
+static inline unsigned long long rdtsc_oai() { 
   unsigned long long a, d;
   __asm__ volatile ("rdtsc" : "=a" (a), "=d" (d));
   return (d<<32) | a;
@@ -29,12 +28,12 @@ static inline unsigned long long rdtsc() {
 static inline void start_meas(time_stats_t *ts) {
 
   ts->trials++;
-  ts->in = rdtsc();
+  ts->in = rdtsc_oai();
 }
 
 static inline void stop_meas(time_stats_t *ts) {
 
-  long long out = rdtsc();
+  long long out = rdtsc_oai();
 
   ts->diff += (out-ts->in);
   if ((out-ts->in) > ts->max)
