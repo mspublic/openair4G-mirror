41bf984b0cd089459a548976bf50a9c4d5254ee8
apostolaras <apostolaras@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-03-18 09:43:40 +0000

    forwardind buffer update

--- branches/lolamesh/openair2/LAYER2/MAC/forwarding_buffer.h
+++ branches/lolamesh/openair2/LAYER2/MAC/forwarding_buffer.h
@@ -1,26 +1,21 @@
-
 #ifndef __FORWARDING_BUFFER__
 #define __FORWARDING_BUFFER__
- 
 
-
-#include "platform_types.h"
-#include "platform_constants.h"
-
-
+//#include "platform_types.h"
+//#include "platform_constants.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// remove after #include "UTIL/MEM/mem_block.h"
-//#include "UTIL/LISTS/list.h"
-
 #define LIST_NAME_MAX_CHAR 32
-
+#define SORT_FIFO 1
+#define SORT_PDU_SEQN 2
+#define SORT_PDU_SIZE 3
+#define MAC_BUFFER_MAXIMUM_CAPACITY 10
 
 //-----------------------------------------------------------------------------
 
-typedef struct mem_block_t{
+typedef struct mem_element_t{
   struct mem_element_t *next;
   struct mem_element_t *previous;
   int seq_num;
@@ -35,44 +30,60 @@ typedef struct {
   struct mem_element_t *head;
   struct mem_element_t *tail;
   
-  int total_size;
-  int    nb_elements;
-  char   name[LIST_NAME_MAX_CHAR];
+  int	total_size;
+  int	nb_elements;
+  char	name[LIST_NAME_MAX_CHAR];
   
-} packet_list_t;
+}packet_list_t;
 
 typedef struct {
  packet_list_t *my_p;
+ u8 sorting_flag;
+ int maximum_capacity;
  char name[LIST_NAME_MAX_CHAR];
-} mac_list_buffer;
+}MAC_BUFFER;
 
-mac_list_buffer **mac_list_buffer_g;
+MAC_BUFFER **mac_buffer_g;
 
-void packet_list_init (packet_list_t* , char *);
-void mac_list_buffer_init(mac_list_buffer * mac_buff, char *, char *); // + flag for buffer type: fifo, sort
+void mac_buffer_top_init();
+MAC_BUFFER *mac_buffer_init(char *nameB, char *nameP, u8 Mod_id, u8 sorting_flag); 
+// sorting_flag denotes the way that packets are sorted when inserted into the mac buffer's packet list (i.e FIFO, pdu_size, seq_num)
+void packet_list_init (packet_list_t*, char *nameP);
+void packet_list_free (packet_list_t* listP);
+void mac_buffer_free(u8 Mod_id);
 
-mem_element_t * packet_list_remove_head (packet_list_t * listP);
-mem_element_t * mac_list_buffer_remove_head(mac_list_buffer *mac_buff); 
+mem_element_t *packet_list_remove_head (packet_list_t *listP);
+mem_element_t *mac_buffer_remove_head(u8 Mod_id); 
 
-mem_element_t * packet_list_remove_tail(packet_list_t * listP); 
-mem_element_t * mac_list_buffer_remove_tail(mac_list_buffer *mac_buff);
+mem_element_t *packet_list_remove_tail(packet_list_t *listP); 
+mem_element_t *mac_buffer_remove_tail(u8 Mod_id);
 
-mem_element_t * packet_list_get_head (packet_list_t * listP); // returns the head of the list without removing it
-mem_element_t * mac_list_buffer_get_head(mac_list_buffer *mac_buff); // returns the head of the list without removing it
+mem_element_t *packet_list_get_head (packet_list_t * listP); // returns the head of the list without removing it!
+mem_element_t *mac_buffer_get_head(u8 Mod_id); // returns the head of the list without removing it!
 
 int packet_list_find_pdu_seq_num(packet_list_t *listP, int seq_num);
-void packet_list_add_tail (mem_element_t * elementP, packet_list_t * listP);
-int mac_list_buffer_add_tail( mac_list_buffer *mac_buff, mem_element_t *elementP);
+int packet_list_find_pdu_seq_num2(packet_list_t *listP, int seq_num);
 
-void mac_list_buffer_print(mac_list_buffer *mac_buf);
+
+void packet_list_add_tail(mem_element_t * elementP, packet_list_t * listP);
+void packet_list_add_head(mem_element_t * elementP, packet_list_t * listP);
+
+void packet_list_add_after_ref(mem_element_t * new_elementP, mem_element_t *elementP_ref, packet_list_t * listP);
+
+int mac_buffer_add_tail(u8 Mod_id, mem_element_t *elementP);
+int mac_buffer_add_after(u8 Mod_id, mem_element_t *elementP); 
+int mac_buffer_add_sorted(u8 Mod_id, mem_element_t *elementP);
+
+void mac_buffer_print(u8 Mod_id);
+void mac_buffer_print_reverse(u8 Mod_id);
+int  mac_buffer_sort(u8 Mod_id);
+
+mem_element_t *packet_list_find_pivot_seq_num(int seq_num, packet_list_t *listP, int *after);
+mem_element_t *packet_list_find_pivot_pdu_size(int pdu_size, packet_list_t *listP, int *after);
 
 // MAC API
-int  mac_list_buffer_total_size(mac_list_buffer *mac_buf); // should be Mod_id
-int  mac_list_buffer_nb_elements(mac_list_buffer *mac_buf); // should be Mod_id
-
-mem_element_t * mac_buffer_data_req( mac_list_buffer *mac_buf, int seq_num, int size, int HARQ_proccess_ID); // 
-//mem_element_t * mac_buffer_data_req(int Mod_id, int seq_num, int size, int HARQ_proccess_ID);
-
-int mac_buffer_data_ind( mac_list_buffer *mac_buf, mem_element_t *elementP, int seq_num, int pdu_size, int HARQ_proccess_ID ); // returns 1 for success otherwise 0; 
-//int mac_buffer_data_ind( int Mod_id, mem_element_t *elementP, int seq_num, int pdu_size, int HARQ_proccess_ID ); // returns 1 for success otherwise 0; // mod_id, add and sort the pdu according to the flag
+int  mac_buffer_total_size(u8 Mod_id);
+int  mac_buffer_nb_elements(u8 Mod_id);
+mem_element_t * mac_buffer_data_req( u8 Mod_id, int seq_num, int size, int HARQ_proccess_ID); 
+int mac_buffer_data_ind( u8 Mod_id, mem_element_t *elementP, int seq_num, int pdu_size, int HARQ_proccess_ID); // returns 1 for success otherwise 0 // returns 1 for success otherwise 0; // mod_id, add and sort the pdu according to the flag
 #endif
