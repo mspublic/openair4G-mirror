f65f50d8c5911da855ccebbe8594517026d80c6f
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-04-26 14:46:14 +0000

    Makefile now accepts RTAI=0 (default RTAI=1), also adding wrapper for rt functions in case RTAI=0 (integration not finished)

---
+++ trunk/targets/RTAI/USER/rt_wrapper.c
@@ -0,0 +1,72 @@
+/*******************************************************************************
+
+  Eurecom OpenAirInterface
+  Copyright(c) 1999 - 2011 Eurecom
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+  Contact Information
+  Openair Admin: openair_admin@eurecom.fr
+  Openair Tech : openair_tech@eurecom.fr
+  Forums       : http://forums.eurecom.fsr/openairinterface
+  Address      : Eurecom, 2229, route des crêtes, 06560 Valbonne Sophia Antipolis, France
+
+*******************************************************************************/
+
+/*! \file rt_wrapper.h
+* \brief provides a wrapper for the timing function for real-time opeartions depending on weather RTAI is used or not
+* \author F. Kaltenberger
+* \date 2013
+* \version 0.1
+* \company Eurecom
+* \email: florian.kaltenberger@eurecom.fr
+* \note
+* \warning
+*/
+
+#include "rt_wrapper.h"
+
+#ifndef RTAI
+
+struct timespec interval, next, now;
+clockid_t clock_id = CLOCK_MONOTONIC; //other options are CLOCK_REALTIME, CLOCK_PROCESS_CPUTIME_ID, CLOCK_THREAD_CPUTIME_ID 
+RTIME rt_get_time_ns (void) {
+  clock_gettime(clock_id, &now);
+  return(now.tv_sec*1e9+now.tv_nsec); 
+}
+
+int rt_sleep_ns (RTIME x) {
+  clock_gettime(clock_id, &now);
+  interval.tv_sec = x/((RTIME)1e9); 
+  interval.tv_nsec = x%((RTIME) 1e9); 
+  next = now;
+  next.tv_sec += interval.tv_sec;
+  next.tv_nsec += interval.tv_nsec;
+  return(clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next, NULL));
+}
+
+#else
+
+RTIME rt_get_time_ns(void) {
+  return(count2nano(rt_get_time()));
+}
+
+void rt_sleep_ns(RTIME x) {
+  rt_sleep(nano2count(x));
+}
+
+#endif
