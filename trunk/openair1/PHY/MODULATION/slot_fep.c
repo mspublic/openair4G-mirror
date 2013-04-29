2ae8ca998cf36aeb51ece357a6e94ed13db13dab
latif <latif@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-04-29 20:56:45 +0000

    fixed a bug for Ideal channel estimation

--- trunk/openair1/PHY/MODULATION/slot_fep.c
+++ trunk/openair1/PHY/MODULATION/slot_fep.c
@@ -152,10 +152,11 @@ int slot_fep(PHY_VARS_UE *phy_vars_ue,
 	   frame_parms->ofdm_symbol_size*sizeof(int));
 #endif
     */
-  }    
+  }
+#ifndef PERFECT_CE    
   if ((l==0) || (l==(4-frame_parms->Ncp))) {
     for (aa=0;aa<frame_parms->nb_antennas_tx_eNB;aa++) {
-#ifndef PERFECT_CE
+
 #ifdef DEBUG_FEP
       msg("Channel estimation eNB %d, aatx %d, slot %d, symbol %d\n",eNB_id,aa,Ns,l);
 #endif
