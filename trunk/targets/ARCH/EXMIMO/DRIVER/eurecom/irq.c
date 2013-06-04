3d6250a4010aa9c6a25f7f36c317db980df039a9
ihmig <ihmig@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-06-04 10:18:48 +0000

    - added support for shared IRQs. requires bitstream that sets control0.bit7 to work reliably

--- trunk/targets/ARCH/EXMIMO/DRIVER/eurecom/irq.c
+++ trunk/targets/ARCH/EXMIMO/DRIVER/eurecom/irq.c
@@ -40,54 +40,56 @@ irqreturn_t openair_irq_handler(int irq, void *cookie)
     // check interrupt status register
     //pci_read_config_word(pdev[0],6 , &irqval);
     
-    // find card_id
+    // find card_id. cookie is set by request_irq and static, so we will always find it
     for (card_id=0; card_id<MAX_CARDS; card_id++)
         if ( pdev[card_id] == cookie )
             break;
-
-    if (card_id == MAX_CARDS) // seems this is not for us. assume card0 and let the "else" branch handle it
-        card_id = 0;
 
     if (exmimo_pci_kvirt[card_id].exmimo_id_ptr->board_swrev == BOARD_SWREV_LEGACY)
         pcie_control = PCIE_CONTROL1;
     else if (exmimo_pci_kvirt[card_id].exmimo_id_ptr->board_swrev == BOARD_SWREV_CMDREGISTERS)
         pcie_control = PCIE_CONTROL2;
 
-
     //printk("irq hndl called: card_id=%i, irqval=%i\n", card_id, irqval);
+
+    // get AHBPCIE interrupt line (bit 7) to determine if IRQ was for us from ExMIMO card, or from a different device
+    // reading CONTROL0 will also clear this bit and the LEON-to-PC IRQ line
+    irqval = ioread32(bar[card_id]+PCIE_CONTROL0);
+
+    irqcmd = ioread32(bar[card_id]+pcie_control);
+    //printk("IRQ handler: ctrl0: %08x, ctrl1: %08x, ctrl2: %08x, status: %08x\n", irqval, ioread32(bar[card_id]+PCIE_CONTROL1), ioread32(bar[card_id]+PCIE_CONTROL2), ioread32(bar[card_id]+PCIE_STATUS));
+    
+    if ( (irqval & 0x80) == 0 )  {  // CTRL0.bit7 is no set -> IRQ is not from ExMIMO i.e. not for us 
+        if ( irqcmd != EXMIMO_NOP ) {
+            static int hide_warning=0;
+            if (hide_warning == 0) {
+                printk("EXMIMO: Warning: unclear source of IRQ: CTL0.bit7 not set, but cmd!=EXMIMO_NOP. Will now execute irqcmd (card %lu, cmd 0x%X). Will hide all future warnings!\n", card_id, irqcmd);
+                hide_warning = 1;
+            }
+        }
+        else
+            return IRQ_NONE;
+    }
     if (exmimo_pci_kvirt[card_id].exmimo_id_ptr->board_swrev == BOARD_SWREV_LEGACY) {
-        // get AHBPCIE interrupt line
-        irqval = ioread32(bar[card_id]+PCIE_CONTROL0);
         // clear PCIE interrupt bit (bit 7 of register 0x0)
         if ( (irqval&0x80) != 0)
             iowrite32(irqval&0xffffff7f,bar[card_id]+PCIE_CONTROL0);
     }
 
-    irqcmd = ioread32(bar[card_id]+pcie_control);   // On ExMIMO2, this ioread is sufficient to clear the IRQ
-
-    //printk("IRQ: ctrl0: %08x, ctrl1: %08x, ctrl2: %08x, status: %08x\n", ioread32(bar[card_id]+PCIE_CONTROL0), ioread32(bar[card_id]+PCIE_CONTROL1), ioread32(bar[card_id]+PCIE_CONTROL2), ioread32(bar[card_id]+PCIE_STATUS));
-    
-    if ( irqcmd != EXMIMO_NOP )
+    if (irqcmd == GET_FRAME_DONE)
     {
-        if (irqcmd == GET_FRAME_DONE)
-        {
-            get_frame_done = 1;
-            //iowrite32(EXMIMO_NOP, bar[card_id]+pcie_control);
-        }
-        else if (irqcmd == PCI_PRINTK)
-        {
-            // TODO: copy printk_buffer into FIFO to allow several consecutive printks and ACK pcie_printk
-            //iowrite32(EXMIMO_NOP, bar[card_id]+pcie_control);
-        }
-        
-        openair_tasklet.data = card_id;
-        tasklet_schedule(&openair_tasklet);
-        openair_bh_cnt++;
-        //printk("PCIE IRQ: openair_irq_handler(irqval=%i): irqcmd = %i (0x%X) (printkbuf: 0x %02ux %02ux\n", irqval, irqcmd, irqcmd, exmimo_pci_kvirt[card_id].printk_buffer_ptr[0], exmimo_pci_kvirt[card_id].printk_buffer_ptr[1]);
+        get_frame_done = 1;
     }
-    else {
-        // other IRQ
+    else if (irqcmd == PCI_PRINTK)
+    {
+        // TODO: copy printk_buffer into FIFO to allow several consecutive printks and ACK pcie_printk
+        //iowrite32(EXMIMO_NOP, bar[card_id]+pcie_control);
     }
+    
+    openair_tasklet.data = card_id;
+    tasklet_schedule(&openair_tasklet);
+    openair_bh_cnt++;
+
     return IRQ_HANDLED;
 }
 
@@ -120,6 +122,7 @@ void openair_do_tasklet (unsigned long card_id)
             break;
             
         case EXMIMO_NOP:
+            printk("[openair][IRQ tasklet] : Received IRQ, with CONTROL0.bit7(IRQ Leon2PC) set, but irqcmd = EXMIMO_NOP. This should not happen with bitstreams built after June 3rd 2013.\n");
             break;
             
         default:
