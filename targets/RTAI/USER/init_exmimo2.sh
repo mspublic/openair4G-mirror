9514b1aaa95e540e6096d16a860e0aef10e80738
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-06-11 15:01:15 +0000

    updating ExpressMIMO2 init script

--- targets/RTAI/USER/init_exmimo2.sh
+++ targets/RTAI/USER/init_exmimo2.sh
@@ -4,14 +4,10 @@ sudo mknod /dev/openair0 c 127 0
 sudo chmod a+rw /dev/openair0
 sleep 1
 
-$OPENAIR_TARGETS/ARCH/EXMIMO/USERSPACE/OAI_FW_INIT/updatefw -s 0x43fffff0 -b -f $OPENAIR0_DIR/express-mimo/software/sdr/exmimo2/sdr_expressmimo2
+#$OPENAIR_TARGETS/ARCH/EXMIMO/USERSPACE/OAI_FW_INIT/updatefw -s 0x43fffff0 -b -f $OPENAIR0_DIR/express-mimo/software/sdr/exmimo2/sdr_expressmimo2
 
-#$OPENAIR_TARGETS/ARCH/EXMIMO/USERSPACE/OAI_FW_INIT/updatefw -s 0x43fffff0 -b -f $OPENAIR_TARGETS/ARCH/EXMIMO/USERSPACE/OAI_FW_INIT/exmimo1_leon 
+$OPENAIR_TARGETS/ARCH/EXMIMO/USERSPACE/OAI_FW_INIT/updatefw -s 0x43fffff0 -b -f $OPENAIR_TARGETS/ARCH/EXMIMO/USERSPACE/OAI_FW_INIT/sdr_expressmimo2
 
-
-#sudo rmmod openair_rf
-#sudo insmod $OPENAIR1_DIR/ARCH/CBMIMO1/DEVICE_DRIVER/openair_rf_softmodem.ko
-
-#sudo rmmod nasmesh
-#sudo insmod $OPENAIR2_DIR/NAS/DRIVER/MESH/nasmesh.ko
+sudo rmmod nasmesh
+sudo insmod $OPENAIR2_DIR/NAS/DRIVER/MESH/nasmesh.ko
 
