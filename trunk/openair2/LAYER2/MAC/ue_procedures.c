beb34123b2eb37ba56dabaea953e28485cb9d5b0
zhou <zhou@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-06-11 11:43:47 +0000

    fix the backoff for the CBA uplink access

--- trunk/openair2/LAYER2/MAC/ue_procedures.c
+++ trunk/openair2/LAYER2/MAC/ue_procedures.c
@@ -1357,13 +1357,15 @@ double uniform_rngen(int min, int max) {
 
 int use_cba_access(u8 Mod_id,u32 frame,u8 subframe, u8 eNB_index){
   
-  if (( ((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID1]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID1] < 21))   ||
+  if ((((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID1]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID1] < 21))   ||
         ((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID2]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID2] < 21))   ||
-        ((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID3]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID3] < 21)) // )){
+        ((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID3]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID3] < 21))) 
 	&& (UE_mac_inst[Mod_id].ul_active == 0) // check if the ul is acrtive
-	&& (UE_mac_inst[Mod_id].cba_last_access[0] <= 0) )) { // backoff
-    
-    UE_mac_inst[Mod_id].cba_last_access[0]= round(uniform_rngen(1,10));
+	&& (UE_mac_inst[Mod_id].cba_last_access[0] <= 0) ) { // backoff
+     LOG_D(MAC,"[UE %d] Frame %d Subframe %d: the current CBA backoff is %d \n", Mod_id, frame, subframe,
+	  UE_mac_inst[Mod_id].cba_last_access[0] ); 
+   
+    UE_mac_inst[Mod_id].cba_last_access[0]= round(uniform_rngen(1,60));
     LOG_D(MAC,"[UE %d] Frame %d Subframe %d: start a new CBA backoff  %d \n", Mod_id, frame, subframe,
 	  UE_mac_inst[Mod_id].cba_last_access[0] );   
     UE_mac_inst[Mod_id].ul_active == 1;
@@ -1374,8 +1376,19 @@ int use_cba_access(u8 Mod_id,u32 frame,u8 subframe, u8 eNB_index){
       UE_mac_inst[Mod_id].cba_last_access[subframe]=0;
       UE_mac_inst[Mod_id].cba_last_access[(subframe+1)%2]=0;
 	*/
-    if (UE_mac_inst[Mod_id].cba_last_access[0]>0)
-      UE_mac_inst[Mod_id].cba_last_access[0]-=1;
+   // if (UE_mac_inst[Mod_id].cba_last_access[0]>0)
+   
+  if ((((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID1]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID1] < 21))   ||
+        ((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID2]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID2] < 21))   ||
+        ((UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID3]> 0 ) && (UE_mac_inst[Mod_id].scheduling_info.BSR[LCGID3] < 21))) 
+	&& (UE_mac_inst[Mod_id].ul_active == 0) // check if the ul is acrtive
+	&& (UE_mac_inst[Mod_id].cba_last_access[0]> 0) )
+  {UE_mac_inst[Mod_id].cba_last_access[0]-=1;
+         LOG_D(MAC,"[UE %d] Frame %d Subframe %d: CBA backoff is decreased by one to %d \n", Mod_id, frame, subframe,
+	  UE_mac_inst[Mod_id].cba_last_access[0] ); 
+    return 0;
+  }
+  
     return 0;
   }
   /*
