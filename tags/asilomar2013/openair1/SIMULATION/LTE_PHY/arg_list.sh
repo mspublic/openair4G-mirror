20a3bb52fba04bac2fee6c9b8055e69b168bf251
kaltenbe <kaltenbe@818b1a75-f10b-46b9-bf7c-635c3b92a50f>
2013-10-24 21:24:42 +0000

    adding new AWGN curves

--- tags/asilomar2013/openair1/SIMULATION/LTE_PHY/arg_list.sh
+++ tags/asilomar2013/openair1/SIMULATION/LTE_PHY/arg_list.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MCS="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27";
+MCS="16 17 18 19 20 21 22 23 24 25 26 27";
 NSIMUS=10000
 
 for M in $MCS
 do
-echo "-x1 -y1 -z1 -a -R1 -s$(($M-7)) -S7 -m$M -n$NSIMUS -f.2"
+echo "-x1 -y1 -z1 -a -R1 -s$(($M-10)) -S7 -m$M -n$NSIMUS -f.2"
 done
